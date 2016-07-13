f4cbdd074a59f8abbce2474e57578c7c170f2a53
Mickaël Perrin <dev@mickaelperrin.fr>
2016-07-13 22:26:11 +0200

    [BUGFIX] Use owner and numericids only when needed

--- fswatch.sh
+++ fswatch.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
 cd $UNISON_DIR
-cmd="fswatch ${FSWATCH_EXCLUDES} -orIE ${UNISON_DIR} | xargs -I -n1 unison $UNISON_EXCLUDES \"$UNISON_DIR\" -auto -batch -owner -numericids socket://${HOST_IP}:${UNISON_HOST_PORT}"
+[ ! -z $UNISON_DIR_OWNER ] && UNISON_MANAGE_OWNER="-owner -numericids" || UNISON_MANAGE_OWNER=""
+cmd="fswatch ${FSWATCH_EXCLUDES} -orIE ${UNISON_DIR} | xargs -I -n1 unison $UNISON_EXCLUDES \"$UNISON_DIR\" -auto -batch $UNISON_MANAGE_OWNER socket://${HOST_IP}:${UNISON_HOST_PORT}"
 echo $cmd
 eval $cmd
