a6690e9373764012fd54ee89d8ea57aeb99256ca
Mickaël Perrin <dev@mickaelperrin.fr>
2016-07-14 00:39:58 +0200

    [BUGFIX] Add missing events to watch

--- fswatch.sh
+++ fswatch.sh
@@ -2,6 +2,6 @@
 set -e
 cd $UNISON_DIR
 [ ! -z $UNISON_DIR_OWNER ] && UNISON_MANAGE_OWNER="-owner -numericids" || UNISON_MANAGE_OWNER=""
-cmd="fswatch ${FSWATCH_EXCLUDES} -orIE ${UNISON_DIR} | xargs -I -n1 unison $UNISON_EXCLUDES \"$UNISON_DIR\" -auto -batch $UNISON_MANAGE_OWNER socket://${HOST_IP}:${UNISON_HOST_PORT}"
+cmd="fswatch ${FSWATCH_EXCLUDES} -orIE --event Updated --event Removed --event AttributeModified --event Created --event Link --event MovedFrom --event MovedTo --event Renamed ${UNISON_DIR} | xargs -I -n1 unison $UNISON_EXCLUDES \"$UNISON_DIR\" -auto -batch $UNISON_MANAGE_OWNER socket://${HOST_IP}:${UNISON_HOST_PORT}"
 echo $cmd
 eval $cmd
